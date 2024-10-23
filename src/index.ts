cd0b695d989b47373dd888f94a1a8b2ef1f6bfd8
Evgeny Lyozin <evgenylyozindev@gmail.com>
2024-10-23 18:30:09 +0300

    rewrite safe timeout

--- src/index.ts
+++ src/index.ts
@@ -27,8 +27,10 @@ export const CreateSafeInterval = (() => {
       const TimeoutID = setTimeout(async () => {
         // wait till the function is fully executed
         await callable(...callableArgs);
-        // and repeat
-        loop();
+        // and repeat if the callable is still available
+        if (FunctionsToClear.has(callable)) {
+          loop();
+        }
       }, timeout);
       FunctionsToClear.set(callable, () => {
         clearTimeout(TimeoutID);
@@ -48,8 +50,8 @@ export const CreateSafeInterval = (() => {
       const Clear = FunctionsToClear.get(callable);
       if (Clear) {
         Clear();
-        FunctionsToClear.delete(callable);
       }
+      FunctionsToClear.delete(callable);
     }
   };
   /**
@@ -95,12 +97,66 @@ export const CreateSafeInterval = (() => {
   };
 })();
 
+/**
+ * Use this function if there is a need to setup multiple intervals for the same callable.
+ * For example, if there is a need to fetch multiple different resources periodically, or the same resource but with different timeouts.
+ * Every call to the function creates a new interval
+ * characteristics which remain from standard CreateSafeInterval:
+ * - no interleaving or shuffling of the callable invocations (inside the same interval)
+ * - predictable clear interval
+ * not like standard CreateSafeInterval:
+ * - creates interval for the same callable each time allowing for not related intervals calling the same callable (no matter the arguments, timeout)
+ */
+export const CreateSafeIntervalMultiple = (
+  callable: (...args: unknown[]) => void,
+  timeout: number | undefined,
+  callableArgs: unknown[],
+) => {
+  // to track single function that is called in the safe interval
+  let Clear: (() => void) | undefined = undefined;
+  /**
+   * Start a new safe interval for the provided function.
+   * @param callable Function to be called at each interval.
+   * @param timeout Interval in milliseconds.
+   * @param callableArgs Arguments for the function.
+   */
+  const startNewSafeInterval = (
+    callable: (...args: unknown[]) => void,
+    timeout: number | undefined,
+    callableArgs: unknown[],
+  ) => {
+    (function loop() {
+      const TimeoutID = setTimeout(async () => {
+        // wait till the function is fully executed
+        await callable(...callableArgs);
+        // and repeat
+        loop();
+      }, timeout);
+      Clear = () => {
+        clearTimeout(TimeoutID);
+      };
+    })();
+  };
+
+  startNewSafeInterval(callable, timeout, callableArgs);
+
+  return Clear;
+};
+
 export const CreateSafeTimeout = (() => {
   // to track all functions that are called in the safe timeout
   const FunctionsToClear: Map<
     (...args: unknown[]) => void,
     (() => void) | undefined
   > = new Map();
+
+  // resolve queue
+  const FunctionsToQueue: Map<
+    (...args: unknown[]) => void,
+    (() => Promise<void>)[]
+  > = new Map();
+  // loop track
+  const FunctionsToLoop: Map<(...args: unknown[]) => void, boolean> = new Map();
   /**
    * Start a new safe timeout for the provided function.
    * @param callable Function to be called at the timeout.
@@ -110,28 +166,21 @@ export const CreateSafeTimeout = (() => {
   const startNewSafeTimeout = (
     callable: (...args: unknown[]) => void,
     timeout: number | undefined,
-    ...callableArgs: unknown[]
+    callableArgs: unknown[],
   ) => {
-    setTimeout(async () => {
-      const Clear = FunctionsToClear.get(callable);
-      if (Clear) {
-        // could be a register callback or destroy callback
-        // in case of a register callback
-        // the call will reregister the timeout with the new arguments and timeout in ms
-        // in case of a destroy callback the call will destroy the timeout completely
-        // in any case the callable will not be actually called here
-        Clear();
-        return;
-      } else {
-        // if here this means that no new timeout request was made for the same callable
-        // after the timeout was created
-        // and no destroy callback was registered before the timeout scheduled the function to be called
-        // then destroy the timeout here
-        if (FunctionsToClear.has(callable)) FunctionsToClear.delete(callable);
+    const TimeoutID = setTimeout(async () => {
+      // push the function into the queue
+      // if the callable is scheduler by the timeout (not cancelled)
+      if (FunctionsToQueue.has(callable)) {
+        FunctionsToQueue.get(callable).push(async () => {
+          await callable(...callableArgs);
+        });
+        startNewTimeoutLoopIfNeeded(callable);
       }
-      // and finally wait till the function is fully executed
-      await callable(...callableArgs);
     }, timeout);
+    FunctionsToClear.set(callable, () => {
+      clearTimeout(TimeoutID);
+    });
   };
 
   /**
@@ -140,62 +189,69 @@ export const CreateSafeTimeout = (() => {
    */
   const registerFunction = (callable: (...args: unknown[]) => void) => {
     FunctionsToClear.set(callable, undefined);
+    if (!FunctionsToQueue.has(callable)) FunctionsToQueue.set(callable, []);
+    if (!FunctionsToLoop.has(callable)) FunctionsToLoop.set(callable, false);
   };
 
-  /**
-   * Creates a register callback that sets up a safe timeout for the given callable function.
-   * The callback, when invoked, registers the function and starts a new timeout for it.
-   * @param callable Function to be executed at the timeout expiration.
-   * @param timeout Timeout duration in milliseconds.
-   * @param callableArgs Additional arguments for the callable function.
-   * @returns A function that, when called, registers and initiates the timeout.
-   */
-  const createRegisterCallback = (
+  const destroySafeInterval = (callable: (...args: unknown[]) => void) => {
+    if (FunctionsToClear.has(callable)) {
+      const Clear = FunctionsToClear.get(callable);
+      if (Clear) {
+        Clear();
+      }
+      FunctionsToClear.delete(callable);
+    }
+  };
+  const startNewTimeoutLoopIfNeeded = (
     callable: (...args: unknown[]) => void,
-    timeout: number | undefined,
-    ...callableArgs: unknown[]
-  ): (() => void) => {
-    return () => {
-      registerFunction(callable);
-      startNewSafeTimeout(callable, timeout, ...callableArgs);
-    };
+  ) => {
+    if (!FunctionsToLoop.get(callable)) {
+      FunctionsToLoop.set(callable, true);
+      (async function loop() {
+        const queue = FunctionsToQueue.get(callable);
+        if (queue.length > 0) {
+          const callable = queue.shift();
+          if (callable) {
+            await callable();
+            loop();
+          }
+        } else {
+          FunctionsToLoop.set(callable, false);
+        }
+      })();
+    }
   };
   /**
-   * Main function to create and manage safe timeout for a given function.
-   * @param callable Function to be called at the timeout.
-   * @param timeout Timeout in milliseconds.
-   * @param callableArgs Arguments for the function.
+   * Creates a register callback that sets up a safe interval for the given callable function.
+   * The callback, when invoked, registers the function and starts a new interval for it.
+   * @param callable Function to be executed at each interval.
+   * @param timeout Interval duration in milliseconds.
+   * @param callableArgs Additional arguments for the callable function.
+   * @returns A function that, when called, registers and initiates the interval.
    */
+  const registerCallable = (
+    callable: (...args: unknown[]) => void,
+    timeout: number | undefined,
+    callableArgs: unknown[],
+  ): void => {
+    destroySafeInterval(callable);
+    registerFunction(callable);
+    startNewSafeTimeout(callable, timeout, callableArgs);
+  };
   return (
     callable: (...args: unknown[]) => void,
     timeout: number | undefined,
-    ...callableArgs: unknown[]
+    callableArgs: unknown[],
   ): (() => void) => {
-    const registerCallback = createRegisterCallback(
-      callable,
-      timeout,
-      callableArgs,
-    );
-    if (FunctionsToClear.has(callable)) {
-      // if the callable has already been registered
-      // this means that the callable has already been scheduled and not finished yet
-      // because if it finished then it should have been destroyed
-      // register the new callback which should run after the currently active callable finishes executing
-      FunctionsToClear.set(callable, registerCallback);
-    } else {
-      // if the callable has not been registered
-      registerCallback();
-    }
+    registerCallable(callable, timeout, callableArgs);
     // in any case return the destroy callback setter
     // which will on invocation set the destroy callback in the map
     // for the callable
-    // the destroy callback if present will be called before the next callable call
-    // on the currently active timeout
+    // the destroy callback if present will be called after the next callable call
+    // on the currently active interval
     // and completely stop the interval and remove the callable from the map
     return () => {
-      if (FunctionsToClear.has(callable)) {
-        FunctionsToClear.set(callable, () => FunctionsToClear.delete(callable));
-      }
+      destroySafeInterval(callable);
     };
   };
 })();
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,5 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
-import { CreateSafeInterval } from "../src/index.js";
+import { CreateSafeInterval, CreateSafeTimeout } from "../src/index.js";
 
 const DataIdentity = (data: string) => {
   return data;
@@ -42,6 +42,15 @@ describe("testing CreateSafeInterval with synchronous function with single argum
     expect(DataIdentityMock).toReturnWith("1000000");
     clear();
   });
+  it("the callable is executed twice if the clear function is called after 2 timeouts passed, meaning the clear stops the interval after 2 ticks", async () => {
+    const clear = CreateSafeInterval(DataIdentityMock, 5000, []);
+    await vi.advanceTimersToNextTimerAsync(); // pass the first tick
+    await vi.advanceTimersByTimeAsync(6000); // pass the second tick + 1000ms (the interval calls the callable)
+    expect(DataIdentityMock).toBeCalledTimes(2);
+    clear(); // remove the interval now
+    await vi.advanceTimersByTimeAsync(1000000); // no more timers should be called
+    expect(DataIdentityMock).toBeCalledTimes(2);
+  });
 });
 
 const AsyncDataIdentity = async (data: string) => {
@@ -65,8 +74,8 @@ const expectedRandomMSArray: number[] = [];
 const actualRandomMSArray: number[] = [];
 const ResolveRandomlySingleIntervalMock = vi.fn(async () => {
   const randMS = (Math.random() * 10 + 2) * 1000;
-  const result = await ResolveRandomly(randMS);
   expectedRandomMSArray.push(randMS);
+  const result = await ResolveRandomly(randMS);
   actualRandomMSArray.push(result);
 });
 
@@ -107,6 +116,22 @@ describe("testing CreateSafeInterval with asynchronous function with single argu
     expect(AsyncDataIdentityMock).toBeCalledWith("1000000");
     clear();
   });
+  it("the callable is executed twice if the clear function is called after 2 timeouts passed, meaning the clear stops the interval after 2 ticks", async () => {
+    const clear = CreateSafeInterval(
+      ResolveRandomlySingleIntervalMock,
+      5000,
+      [],
+    );
+    await vi.advanceTimersToNextTimerAsync(); // pass the first tick
+    await vi.advanceTimersToNextTimerAsync(); // make sure the promise is resolved
+    await vi.advanceTimersByTimeAsync(6000); // pass the second tick (the interval calls the callable but the promise is not resolved yet)
+    expect(ResolveRandomlySingleIntervalMock).toBeCalledTimes(2);
+    clear(); // remove the interval now
+    await vi.advanceTimersToNextTimerAsync(); // here the promise resolves and all the operations should end
+    await vi.advanceTimersByTimeAsync(1000000); // no more timers should be called
+    expect(ResolveRandomlySingleIntervalMock).toBeCalledTimes(2);
+    expect(ResolveRandomlySingleIntervalMock).toHaveResolvedTimes(2);
+  });
   it("no async results mix or overlap if trying to create multiple intervals", async () => {
     for (let i = 0; i < 10; i++) {
       const randMS = (Math.random() * 10 + 2) * 1000;
@@ -133,5 +158,115 @@ describe("testing CreateSafeInterval with asynchronous function with single argu
     expect(ResolveRandomlySingleIntervalMock).toBeCalledTimes(10);
     expect(expectedRandomMSArray).toEqual(actualRandomMSArray);
     clear();
+  });
+});
+
+describe("testing CreateSafeTimeout with synchronous function with single argument", () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+  it("only one timeout for the same callable", async () => {
+    // the callable should be called once with the expected data
+    // only one timeout for the same callable
+    // even with different inputs and timeout
+    CreateSafeTimeout(DataIdentityMock, 0, ["1"]);
+    CreateSafeTimeout(DataIdentityMock, 100000, ["2"]);
+    CreateSafeTimeout(DataIdentityMock, 0, ["3"]);
+    CreateSafeTimeout(DataIdentityMock, 3, ["4"]);
+    const clear = CreateSafeTimeout(DataIdentityMock, 2000, ["5"]);
+    await vi.advanceTimersToNextTimerAsync();
+    expect(DataIdentityMock).toBeCalledTimes(1);
+    expect(DataIdentityMock).toBeCalledWith("5");
+    expect(DataIdentityMock).toReturnWith("5");
+    clear();
+  });
+  it("timeout stops and no callable is executed if the clear function is called before the timeout", async () => {
+    const clear = CreateSafeTimeout(DataIdentityMock, 5000, ["1000000"]);
+    clear();
+    await vi.advanceTimersToNextTimerAsync();
+    expect(DataIdentityMock).toBeCalledTimes(0);
+  });
+  it("the callable is executed fully if the clear function is called after the timeout", async () => {
+    const clear = CreateSafeTimeout(DataIdentityMock, 5000, ["1000000"]);
+    await vi.advanceTimersToNextTimerAsync();
+    expect(DataIdentityMock).toBeCalledTimes(1);
+    expect(DataIdentityMock).toBeCalledWith("1000000");
+    expect(DataIdentityMock).toReturnWith("1000000");
+    clear();
+  });
+});
+
+describe("testing CreateSafeTimeout with asynchronous function with single argument", () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+  it("only one timeout for the same callable", async () => {
+    // the callable should be called once with the expected data
+    // only one timeout for the same callable
+    // even with different inputs and timeout
+    CreateSafeTimeout(AsyncDataIdentityMock, 0, ["1"]);
+    CreateSafeTimeout(AsyncDataIdentityMock, 100000, ["2"]);
+    CreateSafeTimeout(AsyncDataIdentityMock, 0, ["3"]);
+    CreateSafeTimeout(AsyncDataIdentityMock, 3, ["4"]);
+    const clear = CreateSafeTimeout(AsyncDataIdentityMock, 2000, ["5"]);
+    await vi.advanceTimersToNextTimerAsync();
+    await vi.advanceTimersToNextTimerAsync(); // to make sure the promise is resolved
+    expect(AsyncDataIdentityMock).toBeCalledTimes(1);
+    expect(AsyncDataIdentityMock).toBeCalledWith("5");
+    expect(AsyncDataIdentityMock).toHaveResolvedWith("5");
+    clear();
+  });
+  it("timeout stops and no callable is executed if the clear function is called before the timeout", async () => {
+    const clear = CreateSafeTimeout(AsyncDataIdentityMock, 5000, ["1000000"]);
+    clear();
+    await vi.advanceTimersToNextTimerAsync();
+    expect(AsyncDataIdentityMock).toBeCalledTimes(0);
+  });
+  it("the callable is executed fully if the clear function is called after the timeout", async () => {
+    const clear = CreateSafeTimeout(AsyncDataIdentityMock, 5000, ["1000000"]);
+    await vi.advanceTimersToNextTimerAsync();
+    await vi.advanceTimersToNextTimerAsync(); // to make sure the promise is resolved
+    expect(AsyncDataIdentityMock).toBeCalledTimes(1);
+    expect(AsyncDataIdentityMock).toBeCalledWith("1000000");
+    clear();
+  });
+  it("no async results mix or overlap if trying to create multiple timeouts", async () => {
+    const expectedResults: number[] = [];
+    for (let i = 0; i < 10; i++) {
+      const randMS = (Math.random() * 10 + 2) * 1000;
+      expectedResults.push(randMS);
+      const clear = CreateSafeTimeout(ResolveRandomlyMock, 1000, [randMS]);
+      await vi.advanceTimersByTimeAsync(1000); // to start the callable
+      clear();
+    }
+
+    await vi.advanceTimersByTimeAsync(100000); // to make sure the promises are resolved
+
+    expect(ResolveRandomlyMock).toBeCalledTimes(10);
+    expect(expectedResults).toEqual(
+      ResolveRandomlyMock.mock.settledResults.map((r) => r.value),
+    );
+  });
+  it("no async results mix or overlap inside single timeout even if the async operation takes longer than the specified timeout", async () => {
+    for (let i = 0; i < 10; i++) {
+      const clear = CreateSafeTimeout(
+        ResolveRandomlySingleIntervalMock,
+        1000,
+        [],
+      ); // here the timeout is created on every iteration since it is not an interval
+      await vi.advanceTimersByTimeAsync(1000); // to start the callable
+      clear();
+    }
+
+    await vi.advanceTimersByTimeAsync(100000); // to make sure the promises are resolved
+
+    expect(ResolveRandomlySingleIntervalMock).toBeCalledTimes(10);
+    expect(expectedRandomMSArray).toEqual(actualRandomMSArray);
   });
 });
